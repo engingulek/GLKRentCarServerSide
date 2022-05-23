7d779159aa5d3cae573a64f97c4ad1cc6db66fd5
Engin Gülek <engingulek0@gmail.com>
2022-05-23 14:36:21 +0300

    ConnectDatabase

---
+++ GLKRentCarServerSide/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const bodyParser = require("body-parser");
+const cors = require("cors")
+
+app.use(bodyParser.json());
+app.use(cors()); 
+
+
+
+
+
+
+
+mongoose.connection.once("open",()=>{
+    console.log("Connect to DB!")
+
+}).on("error",(error)=>{
+    console.log("Failed to connect" + error)
+})
+
+app.listen(3000,()=> { // server fonksiyonu dinlenmeye başladığında bu callback fonksiyonu çalıştırlacak
+    console.log("Server is running on port 8080")
+    })
